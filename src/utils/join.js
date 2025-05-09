1001fc4b9cc6b173878d90b2586481ec6c1d2fb2
funkjk <kenjift@gmail.com>
2025-05-09 19:55:20 +0900

    patch-for-git-server

--- src/utils/join.js
+++ src/utils/join.js
@@ -1,1 +1,3 @@
-export { join } from 'path-browserify'
+// export { join } from 'path-browserify' 
+// 20250509  patch to run Nodejs
+export { join } from 'path'
--- src/utils/pkg.js
+++ src/utils/pkg.js
@@ -1,5 +1,5 @@
 export const pkg = {
   name: 'isomorphic-git',
-  version: '0.0.0-development',
-  agent: 'git/isomorphic-git@0.0.0-development',
+  version: '0.0.0-development-patch-for-tiny-git-server-patch-for-tiny-git-server',
+  agent: 'git/isomorphic-git@0.0.0-development-patch-for-tiny-git-server-patch-for-tiny-git-server',
 }
